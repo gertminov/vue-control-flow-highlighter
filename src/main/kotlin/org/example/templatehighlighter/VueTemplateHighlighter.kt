1e730444edb9379c3ab548c4d2b16ea4a50cbc0e
Jacob Heim <jacob.heim@mercedes-benz.com>
2026-08-31 22:59:59 +0200

    init

---
+++ src/main/kotlin/org/example/templatehighlighter/VueTemplateHighlighter.kt
@@ -0,0 +1,70 @@
+// VueTemplateHighlighter.kt
+package org.example.templatehighlighter
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlTag
+
+class VueTemplateHighlighter : Annotator {
+    companion object {
+        val TEMPLATE_TAG_KEY =
+            TextAttributesKey.createTextAttributesKey(
+                "VUE_TEMPLATE_TAG",
+                DefaultLanguageHighlighterColors.KEYWORD,
+            )
+        private val LOG = Logger.getInstance(VueTemplateHighlighter::class.java)
+    }
+
+    override fun annotate(
+        element: PsiElement,
+        holder: AnnotationHolder,
+    ) {
+        if (element is XmlTag && element.name == "template") {
+            // dont highlight the top level template tag
+            if (element.parentTag == null) {
+                return
+            }
+
+            // only highlight tags with v- directives
+            if (!element.attributes.any { it.descriptor?.name?.startsWith("v-") == true }) {
+                return
+            }
+
+            // Highlight the opening tag
+            val startTag = element.firstChild
+            val startRange =
+                TextRange(
+                    startTag.textRange.startOffset,
+                    startTag.textRange.startOffset + "<template".length,
+                )
+            holder
+                .newAnnotation(HighlightSeverity.INFORMATION, "Control flow Tag")
+                .range(startRange)
+                .textAttributes(TEMPLATE_TAG_KEY)
+                .create()
+
+            // Highlight the closing tag if it exists
+            val lastChild = element.lastChild
+            if (lastChild.text == ">") {
+                val rangeStart = lastChild.textRange.endOffset - "</template>".length
+                val closeRange =
+                    TextRange(
+                        rangeStart,
+                        lastChild.textRange.endOffset,
+                    )
+                holder
+                    .newAnnotation(HighlightSeverity.INFORMATION, "Control flow Tag")
+                    .range(closeRange)
+                    .textAttributes(TEMPLATE_TAG_KEY)
+                    .create()
+            }
+        }
+//        }
+    }
+}
